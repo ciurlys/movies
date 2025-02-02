b78c4fe4c45f83125317828bb6da00ae36dbb9e4
ciurlys <justas.ciurlys14@gmail.com>
2025-02-02 18:39:38 +0000

    Add CSS to entry pages

--- Movies.Mvc/Hubs/VoteMovieHub.cs
+++ Movies.Mvc/Hubs/VoteMovieHub.cs
@@ -11,13 +11,11 @@ public class VoteMovieHub : Hub
 {
     private readonly MoviesDataContext _db;
     private readonly UserManager<IdentityUser> _userManager;
-	private readonly Logger<VoteMovieHub> _logger;
 
-    public VoteMovieHub (MoviesDataContext db, UserManager<IdentityUser> userManager, Logger<VoteMovieHub> logger)
+    public VoteMovieHub (MoviesDataContext db, UserManager<IdentityUser> userManager)
     {
 	_db = db;
 	_userManager = userManager;
-	_logger = logger;
     }
     
     public async Task SendMovieVote(VoteMovieModel voteMovie)
@@ -29,14 +27,7 @@ public class VoteMovieHub : Hub
 
 	//Get the current votes of the movie
 	var currentMovie = await _db.Movies
-	    .FirstOrDefaultAsync(m => m.MovieId == voteMovie.MovieId);	
-
-	if (currentMovie is null)
-	{
-		_logger.LogWarning("Current movie not found - Movie Id: {MovieId}", voteMovie.MovieId);
-		return;
-	}
-
+	    .FirstOrDefaultAsync(m => m.MovieId == voteMovie.MovieId);
 	try
 	{
 	    var existingVote = await _db.UserVotesMovie
@@ -52,16 +43,16 @@ public class VoteMovieHub : Hub
 		    HasVoted = true
 		};
 		_db.UserVotesMovie.Add(existingVote);
-		currentMovie.Votes++;
+		currentMovie!.Votes++;
 	    }
 	    else if (!existingVote.HasVoted)
 	    {
 		existingVote.HasVoted = true;
-		currentMovie.Votes++;
+		currentMovie!.Votes++;
 	    }
 	    else if (existingVote.HasVoted) //Remove if already voted
 	    {
-		currentMovie.Votes = Math.Max(0, currentMovie.Votes - 1);
+		currentMovie!.Votes = Math.Max(0, currentMovie!.Votes - 1);
 		_db.UserVotesMovie.Remove(existingVote);
 	    }
 	    await _db.SaveChangesAsync();
